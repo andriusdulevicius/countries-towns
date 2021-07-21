bac82668c586242b909c3ebc7a7ccdf66bf0b0d2
AndriusDulevicius <andriusdulevicius@email.com>
2021-07-21 11:02:34 +0300

    api endpoints created

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -5,21 +5,41 @@ const Place = require('../models/Place');
 router.post('/addNewPlace', async (req, res) => {
   console.log(req.body);
 
-  const dummyPlace = {
-    title: 'LosAndzeles',
-    mainLand: 'North America',
-    population: 32000000,
-    countryOrCity: 'city',
-  };
-
-  const newPlace = new Place(dummyPlace);
-
+  const newPlace = new Place(req.body);
   try {
     const result = await newPlace.save();
     res.json(result);
   } catch (err) {
     res.json(err);
   }
+});
+
+router.get('/allPlaces', async (req, res) => {
+  try {
+    const places = await Place.find();
+    res.json(places);
+  } catch (err) {
+    res.status(500).json('internal error');
+  }
+});
+
+router.get('/delete/:id', async (req, res) => {
+  await User.findOneAndDelete({ _id: req.params.id });
+  res.send({ success: true, msg: `Place ${req.body.title} has been deleted.` });
+});
+
+router.put('/edit/:id', async (req, res) => {
+  const { title, mainLand, population, countryOrCity } = req.body;
+  await User.findOneAndUpdate(
+    { _id: id },
+    {
+      title,
+      mainLand,
+      population,
+      countryOrCity,
+    }
+  );
+  res.send({ success: true, msg: `Place ${title} has been updated.` });
 });
 
 module.exports = router;
